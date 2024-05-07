41bd1368d29207a17986327eb083c738328a04cb
himanshuchauhan <himanshurocks721@gmail.com>
2024-05-07 18:59:28 +0530

    uploading tic tac toe game
    ready to plat tic tac toe game using javascript

---
+++ app.js
@@ -0,0 +1,89 @@
+let boxes = document.querySelectorAll(".box")
+let resetBtn = document.querySelector("#reset-btn")
+let newGameBtn = document.querySelector("#new-btn")
+let msgContainer = document.querySelector(".msg-container")
+let msg = document.querySelector("#msg")
+
+let turnO = true;//playerx,playery
+//let arr = ["apple", "banana", "lichi"]
+const winpatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const resetGame=()=>{
+    turnO=true
+    enableBoxes();
+    msgContainer.classList.add("hide")
+    
+}
+
+
+
+
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("box was clicked");
+        if(turnO===true){
+            box.innerText="O"
+            turnO=false
+            
+          
+             
+        }
+        else{
+            box.innerText="X"
+            turnO=true
+        }
+        box.disabled=true
+        checkwinner();
+        
+    })
+
+})
+
+const disableBoxes=()=>{
+    for(let i of boxes){
+        i.disabled=true;
+    }
+}
+
+const enableBoxes=()=>{
+    for(let i of boxes){
+        i.disabled=false;
+        i.innerText=""
+    }
+    
+}
+
+const showWinner=(winner)=>{
+    msg.innerText=`congratulation winner is ${winner}`
+    msgContainer.classList.remove("hide");
+}
+
+const checkwinner=()=> {
+    for(let i of winpatterns){
+        console.log([i[0]],[i[1]],[i[2]]);
+        
+            let pos1val=boxes[i[0]].innerText
+           let pos2val= boxes[i[1]].innerText
+           let pos3val= boxes[i[2]].innerText;
+           if(pos1val!="" && pos2val!=""&& pos3val!=""){
+                if(pos1val===pos2val&&pos2val===pos3val){
+                    console.log("winner!",pos1val);
+                    showWinner(pos1val)
+                    disableBoxes();
+                }
+           }
+    }
+}
+
+newGameBtn.addEventListener("click",resetGame)
+resetBtn.addEventListener("click",resetGame)
+
